b96b59d4dde0f4b45321b9a989587d257c543f8c
Vladimir Ochakovskiy <leo2208948@mail.ru>
2021-09-25 15:37:20 +0700

    Изменен путь файла для vanessa support

--- getSupportObjects.cmd
+++ getSupportObjects.cmd
@@ -1,2 +1,2 @@
 @chcp 65001
-call vanessa-support json --format ДеревоИменМетаданных --src src\cf supportObjects.json
+call vanessa-support json --format ДеревоИменМетаданных --src src\cf tools\json\supportObjects.json
